52fafb5a9bc2d75c0da0d0cca42240d960cc9bd0
BookCatKid <99609593+BookCatKid@users.noreply.github.com>
2026-02-16 23:08:15 -0800

    Add Images.h and more image bitmaps
    Introduce include/Images.h declaring PROGMEM image bitmaps (volume, battery, wifi icons). Move/expand bitmap definitions into src/Images.cpp (remove stray extern from prior definition) so multiple icons are defined in one place. Update src/NowPlaying.cpp to include Images.h and remove the redundant extern declaration. This centralizes icon declarations, stores bitmaps in PROGMEM, and makes the images available to other modules.

---
+++ include/Images.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <Arduino.h>
+
+extern const unsigned char image_volume_normal_bits[] PROGMEM;
+extern const unsigned char image_volume_loud_bits[] PROGMEM;
+extern const unsigned char image_volume_low_bits[] PROGMEM;
+extern const unsigned char image_volume_muted_bits[] PROGMEM;
+
+extern const unsigned char image_battery_0_bits[] PROGMEM;
+extern const unsigned char image_battery_17_bits[] PROGMEM;
+extern const unsigned char image_battery_33_bits[] PROGMEM;
+extern const unsigned char image_battery_67_bits[] PROGMEM;
+extern const unsigned char image_battery_83_bits[] PROGMEM;
+extern const unsigned char image_battery_full_bits[] PROGMEM;
+extern const unsigned char image_battery_charger_connected_bits[] PROGMEM;
+extern const unsigned char image_battery_charging_bits[] PROGMEM;
+
+extern const unsigned char image_wifi_bits[] PROGMEM;
+extern const unsigned char image_wifi_not_connected_bits[] PROGMEM;
--- src/NowPlaying.cpp
+++ src/NowPlaying.cpp
@@ -7,9 +7,9 @@
 #include "NowPlaying.h"
 #include "UIGlobals.h"
 #include "AppLogger.h"
+#include "Images.h"
 
 extern Adafruit_ST7789 tft;
-extern const unsigned char image_volume_normal_bits[];
 
 static bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap) {
     if (y >= tft.height()) return false;
